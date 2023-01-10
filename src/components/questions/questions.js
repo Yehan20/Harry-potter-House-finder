4ce745d606a0b91b258a7d20a4d6ec4388ccc688
Yehan20 <yehansk15@gmail.com>
2023-01-10 06:41:10 +0530

    .

--- src/components/questions/questions.js
+++ src/components/questions/questions.js
@@ -1,34 +1,34 @@
  const  questions = [
   {
     id:1,
-    question:'What do you Mostly love in Life?',
+    question:'What do you love most in life?',
     answers:[{answer:"GLORY",marks:30},{answer:"KNOWLEDGE",marks:20},{answer:"FAME",marks:0}
     ,{answer:'POWER',marks:40},{answer:'FRIENDSHIP',marks:10}]
   },
   {
     id:2,
-    question:'How Do Your Friends Describe You?',
+    question:'How would your friends describe you?',
     answers:[{answer:"PRETTY",marks:0},{answer:"SMART",marks:20},{answer:"LOYAL",marks:10}
       ,{answer:'PROUD',marks:40},{answer:'BRAVE',marks:30}
     ]
   },
   {
     id:3,
-    question:'What of This animals do you Like?',
+    question:'Which of these animals do you like best?',
     answers:[{answer:"SNAKE",marks:40},{answer:"BIRD",marks:30},{answer:"CAT",marks:20}
       ,{answer:'BUG',marks:10},{answer:'MONKEY',marks:0}
     ]
   },
   {
     id:4,
-    question:'What would be the group of your friends at Hogwarts?',
+    question:'What would be the group of friends you have in Hogwarts?',
     answers:[{answer:"POPULAR",marks:40},{answer:"SMART",marks:20},{answer:"WHATEVER I DONT CARE",marks:10}
       ,{answer:'NO FRIENDS',marks:0},{answer:'ATHLETE',marks:30}
     ]
   },
   {
     id:5,
-    question:'What Colors do you Prefer?',
+    question:'What colours do you prefer?',
     answers:[{answer:"PINK & PURPLE",marks:0},{answer:"RED & YELLOW",marks:30},{answer:"GREEN & GREY",marks:40}
       ,{answer:'YELLOW & BLACK',marks:10},{answer:'BLUE & BLACK',marks:20}
     ]
@@ -36,7 +36,7 @@
   ,
   {
     id:6,
-    question:'Which Job you Mostly Identify With',
+    question:'Which job do you most identify with?',
     answers:[{answer:"MEDICAL JOB",marks:10},{answer:"HISTORICAL JOB",marks:20},{answer:"FUN JOB",marks:30}
       ,{answer:'LUCARATIVE',marks:40},{answer:'JOB INVOLING TECH',marks:0}
     ]
@@ -44,7 +44,7 @@
   },
   {
     id:7,
-    question:'Which will you do if a Theif Broke into your House',
+    question:'What would you do if a thief broke into your house?',
     answers:[{answer:"HIDE",marks:20},{answer:"CALL THE COPS",marks:0},{answer:"DO WHAT HE SAYS",marks:10}
       ,{answer:'FIGHT HIM',marks:30},{answer:'ACT AS ANOTHER THEIF',marks:40}
     ]
@@ -52,7 +52,7 @@
   },
   {
     id:8,
-    question:'If you are a Wizard and see a wounded Animal what will you do?',
+    question:'If you are a Wizard and you see a wounded animal, what would you do?',
     answers:[{answer:"HEAL IT",marks:30},{answer:"RUN AWAY",marks:10},{answer:"CALL ANOTHER WIZARD",marks:20}
       ,{answer:'DONT KNOW WHAT TO DO',marks:0},{answer:'MAKE IT DISSAPEAR',marks:40}
     ]
@@ -60,7 +60,7 @@
   },
   {
     id:9,
-    question:'Do you feel Happy Disturbing Children?',
+    question:'Do you feel happy annoying children?',
     answers:[{answer:"YES",marks:30},{answer:"SOMETIMES",marks:0},{answer:"ALWAYS",marks:40}
       ,{answer:'NEVER DONE THIS',marks:10},{answer:'NO',marks:20}
     ]
